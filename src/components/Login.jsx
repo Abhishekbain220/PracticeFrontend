cde7067f5c09f3b637d42419bd42937d6e08920d
abhishekbain220 <you@example.co>
2025-08-16 14:18:31 +0530

    4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,42 @@
 import { useNavigate } from 'react-router-dom';
 import axios from '../utils/axios.jsx';
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { UserContext } from '../utils/UserContext.jsx';
+import Loading from './Loading.jsx';
 
 const Login = () => {
-  let navigate=useNavigate()
-    let { username, setUsername, email, setEmail, password, setPassword,token,setToken } = useContext(UserContext)
-    let submitHandler=async(e)=>{
-        try {
-            e.preventDefault()
-            let user={username,email,password}
-            let createUser=await axios.post("/user/login",user)
-            localStorage.setItem("authToken",JSON.stringify(createUser.data.token))
-            setToken(createUser.data.token)
-            setEmail("")
-            setPassword("")
-            navigate("/")
-        } catch (error) {
-            console.log(error.response.data.message)
-        }
+  let navigate = useNavigate()
+  let { username, setUsername, email, setEmail, password, setPassword, token, setToken } = useContext(UserContext)
+const [LoadingSwitch, setLoadingSwitch] = useState(false)
+  let submitHandler = async (e) => {
+    try {
+      e.preventDefault()
+      setLoadingSwitch(true)
+      let user = { username, email, password }
+      let createUser = await axios.post("/user/login", user)
+      localStorage.setItem("authToken", JSON.stringify(createUser.data.token))
+      setToken(createUser.data.token)
+      setEmail("")
+      setPassword("")
+      navigate("/")
+      setLoadingSwitch(false)
+    } catch (error) {
+      console.log(error.response.data.message)
+      setLoadingSwitch(false)
     }
-    
-  return (
+  }
+
+  return LoadingSwitch == false  ?(
     <div>
       <form action="" onSubmit={submitHandler}>
-            
-            <input type="email" value={email} name="email" onChange={(e)=> setEmail(e.target.value)} placeholder='Enter Email' id="" />
 
-            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} name="password" placeholder='Enter Password' id="" />
-            <button>Login</button>
-        </form>
+        <input type="email" value={email} name="email" onChange={(e) => setEmail(e.target.value)} placeholder='Enter Email' id="" />
+
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" placeholder='Enter Password' id="" />
+        <button>Login</button>
+      </form>
     </div>
-  )
+  ):(<Loading/>)
 }
 
 export default Login
